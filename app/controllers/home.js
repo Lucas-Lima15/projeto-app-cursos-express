673f32aad0446319fb1dc0252f51d6988911f0d5
Lucas-Lima15 <lucasvinilima15@gmail.com>
2021-03-26 14:45:05 -0300

    Criando uma tela para nossa aplicacao

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,7 +1,17 @@
+var cursos = [
+    {nome: 'Unity3D', categoria: 'Jogos'},
+    {nome: 'Express', categoria: 'Back-End'},
+    {nome: 'React', categoria: 'Front-End'},
+]
+
 module.exports = function(app) {
     var controller = {
         index: function(req, res) {
-            res.json({'message': 'Treinaweb'})
+            res.render('index', {cursos});
+        },
+        newItem: function(req, res) {
+            cursos.push(req.body);
+            res.json(cursos);
         }
     }
 
--- config/express.js
+++ config/express.js
@@ -6,6 +6,9 @@ module.exports = function() {
     var app = express();
 
     app.set('port', 3000);
+    app.set('view engine', 'ejs');
+    app.set('views', './app/views');
+
     app.use(express.static('./public'));
     app.use(bodyParser.json());
 
